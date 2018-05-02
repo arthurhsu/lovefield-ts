fb11f154682da6597999f8841ddc7c3ca4dd39ea
Arthur Hsu <artchsu@gmail.com>
2018-05-01 21:12:25 -0700

    Add Global/ServiceId and tests

---
+++ lib/base/global.ts
@@ -0,0 +1,60 @@
+/**
+ * @license
+ * Copyright 2018 The Lovefield Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {ErrorCode, Exception} from './exception';
+import {ServiceId} from './service_id';
+
+export class Global {
+  public static get() {
+    if (!Global.instance) {
+      Global.instance = new Global();
+    }
+    return Global.instance;
+  }
+  private static instance: Global;
+
+  private services: Map<string, object>;
+
+  constructor() {
+    this.services = new Map<string, object>();
+  }
+
+  public clear(): void {
+    this.services.clear();
+  }
+
+  public registerService<T>(serviceId: ServiceId<T>, service: T): T {
+    this.services.set(serviceId.toString(), service as any as object);
+    return service;
+  }
+
+  public getService<T>(serviceId: ServiceId<T>): T {
+    const service = this.services.get(serviceId.toString());
+    if (!service) {
+      throw new Exception(ErrorCode.SERVICE_NOT_FOUND, serviceId.toString());
+    }
+    return service as any as T;
+  }
+
+  public isRegistered<T>(serviceId: ServiceId<T>): boolean {
+    return this.services.has(serviceId.toString());
+  }
+
+  public listServices(): IterableIterator<string> {
+    return this.services.keys();
+  }
+}
---
+++ tests/base/global_test.ts
@@ -0,0 +1,64 @@
+/**
+ * @license
+ * Copyright 2018 The Lovefield Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import * as chai from 'chai';
+import {ErrorCode} from '../../lib/base/exception';
+import {Global} from '../../lib/base/global';
+import {ServiceId} from '../../lib/base/service_id';
+import {TestUtil} from '../../testing/test_util';
+
+const assert = chai.assert;
+
+describe('Global', () => {
+  let global: Global;
+
+  before(() => {
+    global = Global.get();
+  });
+
+  after(() => {
+    global.clear();
+  });
+
+  it('returnSameInstance', () => {
+    const global2 = Global.get();
+    assert.equal(global, global2);
+  });
+
+  it('getService', () => {
+    const serviceId = new ServiceId<object>('whatever');
+    const service = {};
+    global.registerService(serviceId, service);
+    const serviceFromGlobal = global.getService(serviceId);
+    assert.equal(service, serviceFromGlobal);
+    const cache = new ServiceId('cache');
+    assert.isFalse(global.isRegistered(cache));
+
+    TestUtil.assertThrowsError(ErrorCode.SERVICE_NOT_FOUND, () => {
+      global.getService(cache);
+    });
+  });
+
+  it('clear', () => {
+    const serviceId = new ServiceId<object>('whatever');
+    const service = {};
+    global.registerService(serviceId, service);
+    assert.isTrue(global.isRegistered(serviceId));
+    global.clear();
+    assert.isFalse(global.isRegistered(serviceId));
+    assert.deepEqual([], Array.from(global.listServices()));
+  });
+});
