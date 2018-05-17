e91541e5e631b853916c47460e99f51069616915
Arthur Hsu <artchsu@gmail.com>
2018-05-17 14:52:56 +0900

    Add Predicate and PredicateProvider interface.

---
+++ lib/base/predicate.ts
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2016 The Lovefield Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Relation} from '../proc/relation';
+import {Column} from '../schema/column';
+import {Table} from '../schema/table';
+
+export interface Predicate {
+  // Returns relation that holds only the entries satisfying given predicate.
+  eval(relation: Relation): Relation;
+
+  // Reverses the predicate: predicate evaluates to true where before it was
+  // evaluating to false, and vice versa.
+  setComplement(isComplement: boolean): void;
+
+  // Returns a clone of the predicate.
+  copy(): Predicate;
+
+  // Returns an array of all columns involved in this predicate. The optional
+  // results array as the parameter holds previous results, given that this
+  // function is called recursively.  If provided any column will be added on
+  // that array. If not provided a new array will be allocated.
+  getColumns(results?: Column[]): Column[];
+
+  // Returns an array of all tables involved in this predicate. The optional
+  // results array as the parameter holds previous results, given that this
+  // function is called recursively.  If provided any table will be added on
+  // that array. If not provided a new array will be allocated.
+  getTables(results?: Set<Table>): Set<Table>;
+
+  setId(id: number): void;
+  getId(): number;
+}
---
+++ lib/base/predicate_provider.ts
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2016 The Lovefield Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Binder} from '../base/bind';
+import {Column} from '../schema/column';
+import {Predicate} from './predicate';
+
+export type ValueOperandType = Binder|number|string|Date;
+export type OperandType = Column|ValueOperandType;
+
+export interface PredicateProvider {
+  // Equal to
+  eq(operand: OperandType): Predicate;
+
+  // Not equal to
+  neq(operand: OperandType): Predicate;
+
+  // Less than
+  lt(operand: OperandType): Predicate;
+
+  // Less than or equals to
+  lte(operand: OperandType): Predicate;
+
+  // Greater than
+  gt(operand: OperandType): Predicate;
+
+  // Greater than or equals to
+  gte(operand: OperandType): Predicate;
+
+  // JavaScript regex match
+  match(operand: Binder|RegExp): Predicate;
+
+  // Between test: to must be greater or equals to from.
+  between(from: ValueOperandType, to: ValueOperandType): Predicate;
+
+  // Array finding
+  // clang-format off
+  in(values: Binder|ValueOperandType[]): Predicate;
+  // clang-format on
+
+  // Nullity test
+  isNull(): Predicate;
+
+  // Non-nullity test
+  isNotNull(): Predicate;
+}
