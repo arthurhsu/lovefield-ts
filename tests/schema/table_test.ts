032ae3a6e82576d30caede12f17377c28e6bf7f3
Arthur Hsu <artchsu@gmail.com>
2018-05-18 16:43:30 -0700

    Add Table test.

---
+++ tests/schema/table_test.ts
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2016 The Lovefield Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as chai from 'chai';
+import {Database} from '../../lib/schema/database';
+import {getHrDbSchemaBuilder} from '../../testing/hr_schema/hr_schema_builder';
+
+const assert = chai.assert;
+
+describe('Table', () => {
+  const checkAlias = (schema: Database) => {
+    const noAliasTable = schema.table('Job');
+    const name = noAliasTable.getName();
+    const alias = 'OtherJob';
+    const aliasTable = noAliasTable.as(alias);
+
+    assert.isTrue(noAliasTable !== aliasTable);
+
+    // Assertions about original instance.
+    assert.isNull(noAliasTable.getAlias());
+    assert.equal(name, noAliasTable.getName());
+    assert.equal(name, noAliasTable.getEffectiveName());
+
+    // Assertions about aliased instance.
+    assert.equal(alias, aliasTable.getAlias());
+    assert.equal(name, aliasTable.getName());
+    assert.equal(alias, aliasTable.getEffectiveName());
+    assert.equal(noAliasTable.constructor, aliasTable.constructor);
+  };
+
+  it('alias_StaticSchema',
+     () => {
+         // TODO(arthurhsu): To be implemented.
+     });
+
+  it('alias_DynamicSchema', () => {
+    checkAlias(getHrDbSchemaBuilder().getSchema());
+  });
+});
