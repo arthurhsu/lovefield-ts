3bad6ae0b2f9148df8a0cd08fac3fd0ffedb71a8
Arthur Hsu <artchsu@gmail.com>
2018-05-26 13:56:17 -0700

    Add ComparatorFactory and test.

---
+++ lib/index/comparator_factory.ts
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2018 The Lovefield Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Index} from '../schema';
+import {MultiKeyComparator} from './multi_key_comparator';
+import {MultiKeyComparatorWithNull} from './multi_key_comparator_with_null';
+import {SimpleComparator} from './simple_comparator';
+
+export class ComparatorFactory {
+  public static create(indexSchema: Index): SimpleComparator|MultiKeyComparator
+      |MultiKeyComparatorWithNull {
+    if (indexSchema.columns.length === 1) {
+      return new SimpleComparator(indexSchema.columns[0].order);
+    }
+
+    const orders = indexSchema.columns.map((col) => col.order);
+    const nullable = indexSchema.columns.some((col) => col.schema.isNullable());
+    return nullable ? new MultiKeyComparatorWithNull(orders) :
+                      new MultiKeyComparator(orders);
+  }
+}
---
+++ tests/index/comparator_factory_test.ts
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2018 The Lovefield Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as chai from 'chai';
+import {ComparatorFactory} from '../../lib/index/comparator_factory';
+import {MultiKeyComparator} from '../../lib/index/multi_key_comparator';
+import {SimpleComparator} from '../../lib/index/simple_comparator';
+import {getHrDbSchemaBuilder} from '../../testing/hr_schema/hr_schema_builder';
+
+const assert = chai.assert;
+
+describe('ComparatorFactory', () => {
+  const schema = getHrDbSchemaBuilder().getSchema();
+
+  const maxSalary = schema.table('Job').getIndices().filter((index) => {
+    return index.getNormalizedName() === 'Job.idx_maxSalary';
+  })[0];
+  assert.isTrue(
+      ComparatorFactory.create(maxSalary) instanceof SimpleComparator);
+
+  const uqConstraint =
+      schema.table('DummyTable').getIndices().filter((index) => {
+        return index.getNormalizedName() === 'DummyTable.uq_constraint';
+      })[0];
+  assert.isTrue(
+      ComparatorFactory.create(uqConstraint) instanceof MultiKeyComparator);
+});
