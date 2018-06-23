83b829b39cc9fd579750af484241e35d534a7a57
Arthur Hsu <artchsu@gmail.com>
2018-06-22 22:20:30 -0700

    Add GetRowCountStep, SelectStep, TableAccessFullStep.
    Tests can't be added now, will add in later CLs.

---
+++ lib/proc/pp/select_step.ts
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2018 The Lovefield Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {ExecType} from '../../base/private_enum';
+import {Journal} from '../../cache/journal';
+import {Context} from '../../query/context';
+import {Relation} from '../relation';
+import {PhysicalQueryPlanNode} from './physical_query_plan_node';
+
+export class SelectStep extends PhysicalQueryPlanNode {
+  private predicateId: number;
+
+  constructor(predicateId: number) {
+    super(1, ExecType.FIRST_CHILD);
+    this.predicateId = predicateId;
+  }
+
+  public toString(): string {
+    return 'select(?)';
+  }
+
+  public toContextString(context: Context): string {
+    const predicate = context.getPredicate(this.predicateId);
+    return this.toString().replace('?', predicate.toString());
+  }
+
+  public execInternal(
+      relations: Relation[], journal?: Journal, context?: Context): Relation[] {
+    // context must be provided for SelectStep.
+    const predicate = (context as Context).getPredicate(this.predicateId);
+    return [predicate.eval(relations[0])];
+  }
+}
---
+++ lib/proc/pp/table_access_full_step.ts
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2018 The Lovefield Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Global} from '../../base/global';
+import {ExecType} from '../../base/private_enum';
+import {Row} from '../../base/row';
+import {Service} from '../../base/service';
+import {Cache} from '../../cache/cache';
+import {Journal} from '../../cache/journal';
+import {IndexStore} from '../../index/index_store';
+import {RuntimeIndex} from '../../index/runtime_index';
+import {Context} from '../../query/context';
+import {Table} from '../../schema/table';
+import {Relation} from '../relation';
+import {PhysicalQueryPlanNode} from './physical_query_plan_node';
+
+export class TableAccessFullStep extends PhysicalQueryPlanNode {
+  private cache: Cache;
+  private indexStore: IndexStore;
+
+  constructor(global: Global, readonly table: Table) {
+    super(0, ExecType.NO_CHILD);
+    this.cache = global.getService(Service.CACHE);
+    this.indexStore = global.getService(Service.INDEX_STORE);
+  }
+
+  public toString(): string {
+    let postfix = '';
+    if (this.table.getAlias()) {
+      postfix = ` as ${this.table.getAlias()}`;
+    }
+    return `table_access(${this.table.getName()}${postfix})`;
+  }
+
+  public execInternal(
+      relations: Relation[], journal?: Journal, context?: Context): Relation[] {
+    const rowIds =
+        (this.indexStore.get(this.table.getRowIdIndexName()) as RuntimeIndex)
+            .getRange();
+
+    return [Relation.fromRows(
+        this.cache.getMany(rowIds) as any as Row[],
+        [this.table.getEffectiveName()])];
+  }
+}
