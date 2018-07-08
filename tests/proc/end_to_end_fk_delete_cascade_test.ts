2207804739a7057553cb93e47336e36d8a7b1beb
Arthur Hsu <artchsu@gmail.com>
2018-07-08 15:21:02 -0700

    Add end to end foreign key cascade tests.

---
+++ tests/proc/end_to_end_fk_delete_cascade_test.ts
@@ -0,0 +1,164 @@
+/**
+ * Copyright 2018 The Lovefield Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as chai from 'chai';
+
+import {DatabaseConnection} from '../../lib/base/database_connection';
+import {ConstraintAction, DataStoreType, Type} from '../../lib/base/enum';
+import {ErrorCode} from '../../lib/base/exception';
+import {Row} from '../../lib/base/row';
+import {Builder} from '../../lib/schema/builder';
+import {Table} from '../../lib/schema/table';
+
+const assert = chai.assert;
+
+interface SampleRows {
+  tableA: Row[];
+  tableB: Row[];
+  tableB1: Row[];
+  tableB2: Row[];
+}
+
+describe('EndToEndFKDeleteCascade', () => {
+  let db: DatabaseConnection;
+  let tA: Table;
+  let tB: Table;
+  let tB1: Table;
+  let tB2: Table;
+  let sampleRows: SampleRows;
+
+  // Creates a schema that has both CASCADE and RESTRICT constraints as follows.
+  //            TableA
+  //              | Cascade
+  //            TableB
+  //    Cascade /    \ Restrict
+  //           /      \
+  //        TableB1  TableB2
+
+  function getSchemaBuilder(): Builder {
+    const schemaBuilder = new Builder('fk_schema', 1);
+    schemaBuilder.createTable('TableA')
+        .addColumn('id', Type.STRING)
+        .addPrimaryKey(['id']);
+    schemaBuilder.createTable('TableB')
+        .addColumn('id', Type.STRING)
+        .addColumn('foreignId', Type.STRING)
+        .addPrimaryKey(['id'])
+        .addForeignKey('fk_foreignId', {
+          action: ConstraintAction.CASCADE,
+          local: 'foreignId',
+          ref: 'TableA.id',
+        });
+    schemaBuilder.createTable('TableB1')
+        .addColumn('id', Type.STRING)
+        .addColumn('foreignId', Type.STRING)
+        .addPrimaryKey(['id'])
+        .addForeignKey('fk_foreignId', {
+          action: ConstraintAction.CASCADE,
+          local: 'foreignId',
+          ref: 'TableB.id',
+        });
+    schemaBuilder.createTable('TableB2')
+        .addColumn('id', Type.STRING)
+        .addColumn('foreignId', Type.STRING)
+        .addPrimaryKey(['id'])
+        .addForeignKey('fk_foreignId', {
+          action: ConstraintAction.RESTRICT,
+          local: 'foreignId',
+          ref: 'TableB.id',
+        });
+    return schemaBuilder;
+  }
+
+  // Generates one row for each table.
+  function getSampleRows(): SampleRows {
+    return {
+      tableA: [tA.createRow({id: 'tableAId'})],
+      tableB: [tB.createRow({id: 'tableBId', foreignId: 'tableAId'})],
+      tableB1: [tB1.createRow({id: 'tableB1Id', foreignId: 'tableBId'})],
+      tableB2: [tB2.createRow({id: 'tableB2Id', foreignId: 'tableBId'})],
+    };
+  }
+
+  beforeEach(async () => {
+    db = await getSchemaBuilder().connect({storeType: DataStoreType.MEMORY});
+    const schema = db.getSchema();
+    tA = schema.table('TableA');
+    tB = schema.table('TableB');
+    tB1 = schema.table('TableB1');
+    tB2 = schema.table('TableB2');
+    sampleRows = getSampleRows();
+  });
+
+  afterEach(() => db.close());
+
+  // Tests a simple case where a deletion on TableA cascades to TableB and
+  // TableB1.
+  it('cascadeOnlySuccess', async () => {
+    let tx = db.createTransaction();
+    await tx.exec([
+      db.insert().into(tA).values(sampleRows.tableA),
+      db.insert().into(tB).values(sampleRows.tableB),
+      db.insert().into(tB1).values(sampleRows.tableB1),
+    ]);
+
+    await db.delete().from(tA).exec();
+
+    tx = db.createTransaction();
+    const results = await tx.exec(
+        [db.select().from(tA), db.select().from(tB), db.select().from(tB1)]);
+
+    assert.equal(0, results[0].length);
+    assert.equal(0, results[1].length);
+    assert.equal(0, results[2].length);
+  });
+
+  // Test the case where a deletion on TableA, cascades to TableB and TableB1,
+  // but because TableB2 refers to TableB with a RESTRICT constraint, the entire
+  // operation is rejected.
+  it('runCascadeRestrictFail', async () => {
+    let tx = db.createTransaction();
+    await tx.exec([
+      db.insert().into(tA).values(sampleRows.tableA),
+      db.insert().into(tB).values(sampleRows.tableB),
+      db.insert().into(tB1).values(sampleRows.tableB1),
+      db.insert().into(tB2).values(sampleRows.tableB2),
+    ]);
+
+    let failed = true;
+    try {
+      await db.delete().from(tA).exec();
+    } catch (e) {
+      // 203: Foreign key constraint violation on constraint {0}.
+      assert.equal(ErrorCode.FK_VIOLATION, e.code);
+
+      tx = db.createTransaction();
+      const res = await tx.exec([
+        db.select().from(tA),
+        db.select().from(tB),
+        db.select().from(tB1),
+        db.select().from(tB2),
+      ]);
+
+      assert.equal(1, res[0].length);
+      assert.equal(1, res[1].length);
+      assert.equal(1, res[2].length);
+      assert.equal(1, res[3].length);
+      failed = false;
+    }
+    assert.isFalse(failed);
+  });
+});
---
+++ tests/proc/end_to_end_fk_update_cascade_test.ts
@@ -0,0 +1,179 @@
+/**
+ * Copyright 2018 The Lovefield Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as chai from 'chai';
+
+import {DatabaseConnection} from '../../lib/base/database_connection';
+import {ConstraintAction, DataStoreType, Type} from '../../lib/base/enum';
+import {Row} from '../../lib/base/row';
+import {op} from '../../lib/fn/op';
+import {Builder} from '../../lib/schema/builder';
+import {Table} from '../../lib/schema/table';
+
+const assert = chai.assert;
+
+interface SampleRows {
+  tableA: Row[];
+  tableB: Row[];
+}
+
+describe('EndToEndFKUpdateCascade', () => {
+  let db: DatabaseConnection;
+  const options = {storeType: DataStoreType.MEMORY};
+
+  afterEach(() => db.close());
+
+  it('update1FK', async () => {
+    db = await getSchemaBuilder1().connect(options);
+    const schema = db.getSchema();
+    const tA = schema.table('TableA');
+    const tB = schema.table('TableB');
+    const sampleRows = getSampleRows1(tA, tB);
+
+    const updatedId = 'newTableAId0';
+
+    let tx = db.createTransaction();
+    await tx.exec([
+      db.insert().into(tA).values(sampleRows.tableA),
+      db.insert().into(tB).values(sampleRows.tableB),
+    ]);
+    await db.update(tA)
+        .set(tA['id'], updatedId)
+        .where(tA['id'].eq(sampleRows.tableA[0].payload()['id']))
+        .exec();
+
+    tx = db.createTransaction();
+    const results = await tx.exec([
+      db.select().from(tA).where(tA['id'].eq(updatedId)),
+      db.select().from(tB).where(tB['foreignId'].eq(updatedId)),
+    ]);
+
+    assert.equal(1, results[0].length);
+    assert.equal(2, results[1].length);
+  });
+
+  function getSchemaBuilder1(): Builder {
+    const schemaBuilder = new Builder('fk_schema', 1);
+    schemaBuilder.createTable('TableA')
+        .addColumn('id', Type.STRING)
+        .addPrimaryKey(['id']);
+    schemaBuilder.createTable('TableB')
+        .addColumn('id', Type.STRING)
+        .addColumn('foreignId', Type.STRING)
+        .addPrimaryKey(['id'])
+        .addForeignKey('fk_foreignId', {
+          action: ConstraintAction.CASCADE,
+          local: 'foreignId',
+          ref: 'TableA.id',
+        });
+    return schemaBuilder;
+  }
+
+  // Generates two rows for TableA and four rows for TableB, where there are two
+  // rows referring to each row in TableA.
+  function getSampleRows1(tA: Table, tB: Table): SampleRows {
+    const rows: SampleRows = {tableA: [], tableB: []};
+
+    for (let i = 0; i < 2; i++) {
+      rows.tableA.push(tA.createRow({id: `tableAId${i}`}));
+
+      for (let j = 0; j < 2; j++) {
+        rows.tableB.push(tB.createRow({
+          foreignId: rows.tableA[i].payload()['id'],
+          id: 'tableBId' + rows.tableB.length,
+        }));
+      }
+    }
+    return rows;
+  }
+
+  it('update2FK', async () => {
+    db = await getSchemaBuilder2().connect(options);
+    const schema = db.getSchema();
+    const tA = schema.table('TableA');
+    const tB = schema.table('TableB');
+    const sampleRows = getSampleRows2(tA, tB);
+
+    const updatedId1 = 7;
+    const updatedId2 = 8;
+
+    let tx = db.createTransaction();
+    await tx.exec([
+      db.insert().into(tA).values(sampleRows.tableA),
+      db.insert().into(tB).values(sampleRows.tableB),
+    ]);
+
+    await db.update(tA)
+        .set(tA['id1'], updatedId1)
+        .set(tA['id2'], updatedId2)
+        .where(op.and(
+            tA['id1'].eq(sampleRows.tableA[0].payload()['id1']),
+            tA['id2'].eq(sampleRows.tableA[0].payload()['id2'])))
+        .exec();
+
+    tx = db.createTransaction();
+    const results = await tx.exec([
+      db.select().from(tA).where(
+          op.and(tA['id1'].eq(updatedId1), tA['id2'].eq(updatedId2))),
+      db.select().from(tB).orderBy(tB['id']),
+    ]);
+
+    assert.equal(1, results[0].length);
+    assert.equal(3, results[1].length);
+    assert.equal(updatedId1, results[1][0]['foreignId1']);
+    assert.equal(updatedId2, results[1][0]['foreignId2']);
+    assert.equal(updatedId2, results[1][1]['foreignId2']);
+  });
+
+  function getSchemaBuilder2(): Builder {
+    const schemaBuilder = new Builder('fk_schema2', 1);
+    schemaBuilder.createTable('TableA')
+        .addColumn('id1', Type.INTEGER)
+        .addUnique('uq_id1', ['id1'])
+        .addColumn('id2', Type.INTEGER)
+        .addUnique('uq_id2', ['id2']);
+    schemaBuilder.createTable('TableB')
+        .addColumn('id', Type.INTEGER)
+        .addColumn('foreignId1', Type.INTEGER)
+        .addColumn('foreignId2', Type.INTEGER)
+        .addForeignKey('fk_foreignId1', {
+          action: ConstraintAction.CASCADE,
+          local: 'foreignId1',
+          ref: 'TableA.id1',
+        })
+        .addForeignKey('fk_foreignId2', {
+          action: ConstraintAction.CASCADE,
+          local: 'foreignId2',
+          ref: 'TableA.id2',
+        });
+    return schemaBuilder;
+  }
+
+  function getSampleRows2(tA: Table, tB: Table): SampleRows {
+    return {
+      tableA: [
+        tA.createRow({id1: 1, id2: 4}),
+        tA.createRow({id1: 2, id2: 5}),
+        tA.createRow({id1: 3, id2: 6}),
+      ],
+      tableB: [
+        tB.createRow({id: 0, foreignId1: 1, foreignId2: 4}),
+        tB.createRow({id: 1, foreignId1: 2, foreignId2: 4}),
+        tB.createRow({id: 2, foreignId1: 3, foreignId2: 6}),
+      ],
+    };
+  }
+});
